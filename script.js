d2b3f25d76dc4b977e34b21c4d67784a03d8c1b8
Jagdeep Tiwana <jtiwana@hotmail.com>
2025-01-18 20:04:17 +0000

    Added html pages, css files and js file

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+Project: Project 2 - Shop Front
+File: script.js
+Created Date: 11/01/2025
+modified Date: 18/01/2025
+Author: Jagdeep Tiwana
+*/
+
+const toggle = document.querySelectorAll(".icon-container");
+const body = document.querySelector("body");
+const navItems = document.querySelector(".nav-items");
+const navToggle = document.querySelector(".toggle");
+const toggleActive = document.querySelector(".toggleActive");
+
+toggle.forEach((item) => {
+  item.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    toggle.style.visability = "hidden";
+  });
+});
+
+navToggle.addEventListener("click", () => {
+  navItems.classList.toggle("navisActive");
+  navToggle.classList.toggle("toggleActive");
+});
